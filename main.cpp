6b169c51097bf64ea432165a7b964b5c4eb57f6d
yusufoner77 <yusufoner7@gmail.com>
2024-02-04 15:29:30 -0600

    hopefully adding my code to this branch

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath>
+#include "prime.hpp"
+using namespace std;
+
+int main() {
+
+    int input;
+
+    cout << "Enter a number greater than 1: ";
+    cin >> input;
+    cout << endl;
+
+    while(input <= 1) {
+
+        cout << "I cannot calculate the prime factors of that number." << endl << endl;
+
+        cout << "Enter a number greater than 1: ";
+        cin >> input;
+        cout << endl;
+
+    }
+
+    prime (input);
+
+}
